4ea656da971aeadc39070ccfe5c10a121ce57e92
Sharayu_Darandale <168580698+sharayudarandale@users.noreply.github.com>
2025-04-02 16:41:23 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,33 @@
+public class loops {
+    public static void main(String[] args) {
+       
+
+        // for loop
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+    }
+            // while loop
+            int x=0;
+            while(x<10){
+                System.out.println(x);
+                x=x+1;
+            }
+            // do while loop
+            int j=0;
+           
+            do{
+                System.out.println(j);
+                j=j+1;
+
+            }while(j<11);
+
+            int sum=0;
+            int n=10;
+            for(int i=0;i<=n;i++){
+                sum =sum+i;
+            }
+            System.out.println(sum);
+        }
+    
+    }
+
