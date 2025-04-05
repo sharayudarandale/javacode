40196df30459ad24363a1ce00911b757fc9e70dd
Sharayu_Darandale <168580698+sharayudarandale@users.noreply.github.com>
2025-04-05 11:13:59 +0530

    Add files via upload

---
+++ wrapperClass.java
@@ -0,0 +1,14 @@
+
+
+
+public class wrapperClass {
+    public static void main(String[] args) {
+        String str="10";
+        int num=Integer.parseInt(str);
+        System.out.println(num*2);
+
+       
+   
+
+    }
+}
